663159e660095beed76a4a1c780aefee752af5c3
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-03-18 10:03:15 -0300

    first commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+
+import { MovieCard } from './MovieCard';
+
+export function Content({selectedGenre,movies}) {
+  return (
+     <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,22 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+export function SideBar({genres,selectedGenreId,handleClickButton}) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
